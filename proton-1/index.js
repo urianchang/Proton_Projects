0417fe3cb4fda11ba965f254d2ead79a0669e2e6
Urian <uchang@sightmachine.com>
2018-05-18 00:55:25 -0700

    Very basics of Ninja Gold implemented

--- proton-1/index.js
+++ proton-1/index.js
@@ -1,17 +1,99 @@
-import React, { Component } from 'react'; // import from react
+import React, { Component } from 'react';
+import {
+  render,
+  Window,
+  App,
+  Form,
+  Text,
+  TextInput,
+  ProgressBar,
+  Area,
+  Button,
+  Box,
+  Menu,
+} from 'proton-native';
 
-import { render, Window, App } from 'proton-native'; // import the proton-native components
 
-class Example extends Component {
-  render() { // all Components must have a render method
+class MainMenu extends Component {
+  render() {
+    return(
+      <Menu label="File">
+        <Menu.Item
+          type="Item"
+          children="Exit"
+          onClick={this.props.closeWindow}
+          />
+      </Menu>
+    );
+  }
+}
+
+
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gold: 0,
+      isClosed: false,
+    }
+    this.closeWindow = this.closeWindow.bind(this);
+    this.makeGold = this.makeGold.bind(this);
+  }
+
+  closeWindow() {
+    this.setState({isClosed: true})
+  }
+
+  makeGold(location) {
+    const current_gold = this.state.gold;
+    let gold_earned = 0;
+    switch (location) {
+      case "Farm":
+        gold_earned = 10;
+        break;
+      case "Casino":
+        gold_earned = -10;
+        break;
+      case "Dojo":
+        gold_earned = 20;
+        break;
+      default:
+        gold_earned = 0;
+    }
+    const gold = current_gold + gold_earned;
+    this.setState({gold});
+  }
+
+  render() {
+    const { isClosed, gold } = this.state;
     return (
-      <App> // you must always include App around everything
-        <Window title="Proton Native Rocks!" size={{w: 300, h: 300}} menuBar={false}>
-            {/* all your other components go here*/}
+      <App>
+        <MainMenu
+          closeWindow={this.closeWindow}
+          />
+        <Window
+          title="Ninja Gold!"
+          size={{w: 500, h: 500}}
+          menuBar={true}
+          closed={isClosed}
+          >
+          <Box>
+            <Text>{`You have: ${gold} gold pieces`}</Text>
+            <Button onClick={this.makeGold}>
+              Farm
+            </Button>
+            <Button onClick={this.makeGold}>
+              Casino
+            </Button>
+            <Button onClick={this.makeGold}>
+              Dojo
+            </Button>
+          </Box>
         </Window>
       </App>
     );
   }
 }
 
-render(<Example />); // and finally render your main component
+render(<Main />);
